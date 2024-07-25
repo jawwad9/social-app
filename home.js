b9df3f9b7a0d173588ade763c502d7775e9ec33d
jawwad9 <154079885+jawwad9@users.noreply.github.com>
2024-07-26 02:03:35 +0500

    Add files via upload
    Login and logout assignment completed

---
+++ home.js
@@ -0,0 +1,23 @@
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { auth } from "./config.js";
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      console.log(uid)
+    } else {
+        window.location = 'index.html'
+    }
+  });
+
+const logout = document.querySelector('#logout-btn')
+
+logout.addEventListener('click', () => {
+    signOut(auth).then(() => {
+        console.log('logout Successfully')
+        window.location = 'index.html'
+      }).catch((error) => {
+
+        console.log('error')
+    });
+})
